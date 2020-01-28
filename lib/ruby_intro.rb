7529e1681968d69a1bf64dfce49f070d5f38b6b5
EC2 Default User <ec2-user@ip-172-31-4-226.us-east-2.compute.internal>
2020-01-28 00:29:26 +0000

    1st

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,94 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  summ = 0
+  for i in arr
+  summ = summ+i
+  end
+return summ
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.length>1
+    return arr.sort[-1]+ arr.sort[-2]
+  elsif arr.length==0
+  return 0
+  else return arr[0]
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  map = {}
+  for i in arr
+    num = n-i
+    if map.keys.include?(num)
+      return TRUE
+    else
+      map[i] = 1
+    end
+  end    
+  return FALSE
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if /[aeiouAEIOU]/.match(s[0])
+    return false
+  elsif /[^a-zA-Z0-9.*]/.match(s)
+    return false
+  elsif s.length==0
+    return false
+  else
+    return true
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  if s =~ /[a-zA-Z]/
+    return false
+  elsif s.length<1
+    return false
+  end
+  return s.to_i(2)%4 ==0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn, price)
+    unless isbn.length>0 && price>0
+    raise ArgumentError.new("ArgumentError")
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price 
+    @price
+  end
+  
+  def isbn 
+    @isbn
+  end
+  
+  def price=(price) 
+    @price = price
+  end
+  def isbn=(isbn) 
+    @isbn = isbn
+  end
+  def price_as_string
+    p = '%.2f' % @price 
+    return "$#{p}"  
+  end
+
+
+
 end
